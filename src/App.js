3bcd7fd40527795ae7bf8274855f8cb1398a2a54
Aritroo Chowdhury <aritroo123chowdhury52@gmail.com>
2023-07-25 22:07:03 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-   <div>
-       <h1 className="text-3xl font-bold underline text-cyan-50">
-      Hello world!
-    </h1>
-   </div>
+    <div>
+      <h1 className="text-3xl font-bold underline text-cyan-50">Hello world</h1>
+    </div>
   );
 }
 
