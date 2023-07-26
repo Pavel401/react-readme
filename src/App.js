ca0c1bccdf218443718d9df0b01fe41c54be6c2e
Pavel Alam <pavelalam401@gmail.com>
2023-07-27 02:46:35 +0530

    📦 NEW: Navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './components/sidebar';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-   <div>
-       <h1 className="text-3xl font-bold underline text-cyan-50">
-      Hello world!
-    </h1>
-   </div>
+    <div className="h-screen flex flex-col">
+      <Navbar />
+      <div className="flex flex-1 overflow-y-scroll">
+        <Sidebar />
+        <div className="w-1/4 bg-blue-200">
+          {/* Your content here */}
+        </div>
+        <div className="w-1/2 bg-green-200">
+          {/* Your content here */}
+        </div>
+      </div>
+    </div>
   );
 }
 
